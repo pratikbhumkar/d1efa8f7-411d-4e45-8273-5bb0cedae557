91c68abecc3bf46d4f46feb20e60e04e9fbce4e9
Pratik Bhumkar <prateekbhumkar@gmail.com>
2021-02-08 00:38:06 +1100

    Adding basic test cases

--- tests/increasingsequence.test.js
+++ tests/increasingsequence.test.js
@@ -1,7 +1,13 @@
-import findIncreasingSubArray from "../index.js";
+import IncreasingSubArrayRunner from "../index.js";
 
 describe('Testing application for inputs', () => {
-    it('It imports', () => {
-        findIncreasingSubArray()
+    it('Test Case 01', () => {
+        expect(IncreasingSubArrayRunner('6 1 5 9 2')).toBe('1 5 9');
+    });
+    it('Test Case 10', () => {
+        expect(IncreasingSubArrayRunner('6 2 4 6 1 5 9 2')).toBe('2 4 6');
+    });
+    it('Test Case 11', () => {
+        expect(IncreasingSubArrayRunner('6 2 4 3 1 5 9')).toBe('1 5 9');
     });
 })
