6955b503bac1d122a6b08bb98b774cc8fde1fbdd
Pratik Bhumkar <prateekbhumkar@gmail.com>
2021-02-08 01:41:30 +1100

    Adding increasing subarray logic

--- IncreasingSubarrayFinder.js
+++ IncreasingSubarrayFinder.js
@@ -1,4 +1,24 @@
 export default function findIncreasingSubArray(inputArray) {
-    
-    return inputArray;
+    var longestIncreasingSubArray = [];
+    var longestIncreasingSubArrayCount = 0;
+    var currentElement = 0;
+    var currentArray = [];
+    var currentArrayCount = 0;
+    for (let index = 0; index < inputArray.length; index++) {
+        if (Number(inputArray[index]) > Number(currentElement)) {
+            currentElement = inputArray[index];
+            currentArray.push(inputArray[index])
+            currentArrayCount++;
+            if (currentArrayCount>longestIncreasingSubArrayCount && currentArrayCount > 1) {
+                longestIncreasingSubArrayCount = currentArrayCount
+                longestIncreasingSubArray = currentArray
+            }
+        } else {
+            currentArrayCount = 0;
+            currentArray = [];
+            currentElement = 0;
+            index--;
+        }
+    }
+    return longestIncreasingSubArray;
 }
